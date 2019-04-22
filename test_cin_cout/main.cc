7772268d5d00c9b1b1bf64ba29511ce74a9084fd
rt123 <rt123@localhost.localdomain>
2019-04-22 12:30:13 +0800

    test use cin and cout

---
+++ test_cin_cout/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "enter tow numbers" << std::endl;
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << a << "+" << b << "is" << a + b << std::endl;
+}
