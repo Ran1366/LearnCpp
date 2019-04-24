57c978f2a94c196241cbaf37e1a16f2b88c008ff
rt123 <rt123@localhost.localdomain>
2019-04-24 17:23:50 +0800

    生成空指针的注意事项

---
+++ test_point/main.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+  int test = 0;
+  int *pt;
+  //给指针pt赋0值, 不能用变量直接赋值
+  //pt = test;
+  //可以使用以下方法生成空指针
+  //int *pt = nullptr
+  //int *pt = 0;
+  //#include <cstdlib>
+  //int *pt = NULL
+  
+  return 0;
+}
