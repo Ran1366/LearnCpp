9288e06ba475491ace0fd5b1d150c999eb921240
Ran1366 <m13669286873_1@163.com>
2019-04-26 09:46:35 +0800

    string的常见用法

--- test_string/main.cpp
+++ test_string/main.cpp
@@ -14,9 +14,53 @@ int main()
   string s4("string");//s5是字面值常量"string"的副本,除了字面值最后那个空字符外
   string s5 = "string";//等价于s4("string"),s3是字面值常量"string"的副本
   string s6(10, 's');//把s6初始化为由连续10个字符s组成的串
-  
-  string s7 = "hello world";
-  cout << s7 << endl;
+
+  //string的操作
+  //is >> s 将is中读取字符串赋给s, 字符串以空白符分隔, 返回is
+  cin >> s1;
+  //os << s 将s写到输出流os中, 返回os
+  cout << s1 << endl;
+  //getline(is, s)  从is中读取一行赋给s, 返回is
+  getline(cin, s1);
+  //s.empty() s为空返回ture, 否则返回false
+  bool re = s1.empty();
+  if(re)
+  {
+    cout << re << endl;
+  }
+  //s.size() 返回s中字符的个数
+  auto size = s1.size();
+  if(size)
+  {
+    cout << "size = " << size << endl;
+  }
+  //s[n] 返回s中第n个字符的引用, 位置n从0开始
+  char n = s1[0];
+  if(n)
+  {
+    cout << "s1[0] = " << n << endl;
+  }
+  //s1 + s2   返回s1和s2连接后的结果
+  string ss = s1 + s2;
+  cout << "ss = " << ss << endl;
+  //s1 = s2   用s2的副本代替s1中原来的字符
+  s1 = s2;
+  cout << "s1 = " << s1 << endl;
+  //s1 == s2  如果s1的内容和s2相等则返回ture, 否则返回false, 判断对大小写敏感
+  if(s1 == s2)
+  {
+    cout << "s1 = s2" << endl;
+  }
+  //s1 != s2  判断是否不相等, 对大小写敏感
+  if(s1 != s2) 
+  {
+    cout << "s1 != s2" << endl;
+  }
+  //<, <=, >, >=  利用字典序进行比较, 对字母大小写敏感
+  if(s1 > s2) 
+  {
+    cout << "s1 > s2" << endl;
+  }
 
   //循环读取字符串, 直到文件结束
   while(cin >> s1)
